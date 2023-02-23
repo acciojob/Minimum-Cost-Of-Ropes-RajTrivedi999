ff4549982e27676bed71eedc7267d6cc5fec1fd2
raj trivedi <113424414+RajTrivedi999@users.noreply.github.com>
2023-02-23 19:44:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your code here
+      function clickMe(event) {
+        event.preventDefault()
+		var newArr=document.getElementsByTagName('input')[0].value
+	      var arr=newArr.split(',')
+	      var sum=0;
+	      var r=document.getElementById('result')
+	      while(arr.length>1){
+		  
+        arr.sort(function(a,b){return a-b})
+        var newElement=Number(arr[0])+Number(arr[1])
+        sum+=newElement
+        arr.splice(0,2)
+        arr.unshift(newElement)
+	  }
+        r.innerText=sum
+      }
